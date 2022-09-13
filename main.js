be5b0ab1e7ff57e98f73918a7e17fed45a0346b5
SoCalStyles <stylesaaronk@gmail.com>
2022-09-12 20:38:10 -0700

    add func to generate grid based on user prompt

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const body = document.querySelector('body');
 const bigContainer = document.querySelector('.bigContainer');
+const generateButton = document.querySelector('.generateButton');
 
 // create 16x16 grid of square divs
 function createTiles() {
@@ -16,6 +17,34 @@ function createTiles() {
 }
 
 createTiles();
+
+function generateGrid() {
+    let answer = prompt('How many squares would you like on each side?');
+    answer = +(answer);
+    if (answer > 100) {
+        alert('That number is too big.');
+    } else if (answer <= 0) {
+        alert('That number is too small.');
+    } else {
+        for (let i = 0; i < answer; i++) {
+            const tileContainer = document.createElement('div');
+            bigContainer.appendChild(tileContainer);
+            tileContainer.classList.add('tileContainer');
+            for (let i = 0; i < answer; i++) {
+                const tile = document.createElement('div');
+                tileContainer.appendChild(tile);
+                tile.classList.add('singleTile');
+            }
+        }
+    }
+}
+
+generateButton.addEventListener('click', () => {
+    // remove original 16x16 grid
+    bigContainer.textContent = "";
+    generateGrid();
+});
+
 
 
 
